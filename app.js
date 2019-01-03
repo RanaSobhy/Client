b46fa5bc4c1e98e9736c888eabcf3d0467f7a9e7
Rana Sobhy <Rana.Sobhy@itworx.com>
2019-01-03 11:39:15 +0200

    request a page from api

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 const xlsx = require("node-xlsx").default;
+const axios = require("axios");
 
 const workSheetsFromFile = xlsx.parse(`${__dirname}/books.xlsx`);
 const books = workSheetsFromFile[0].data;
 const authors = workSheetsFromFile[1].data;
+
+axios
+  .post("http://localhost:3000/api/categories")
+  .then(function(response) {
+    console.log(response.data);
+  })
+  .catch(function(error) {
+    console.log(error);
+  });
