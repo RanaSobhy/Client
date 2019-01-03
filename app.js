9f9e9ff1c86c285defc5397d0d9d917a737286eb
Rana Sobhy <Rana.Sobhy@itworx.com>
2019-01-03 16:06:02 +0200

    create book object

--- app.js
+++ app.js
@@ -2,18 +2,36 @@ const xlsx = require("node-xlsx").default;
 const axios = require("axios");
 const errorHandler = require("./Handler/error");
 
-const workSheetsFromFile = xlsx.parse(`${__dirname}/books.xlsx`);
-const books = workSheetsFromFile[0].data;
-const authors = workSheetsFromFile[1].data;
+const workbook = xlsx.parse(`${__dirname}/books.xlsx`);
+const books = workbook[0].data;
+const authors = workbook[1].data;
 
-(async () => {
-  try {
-    var response = await axios.post("http://localhost:3000/api/categories");
-    console.log(response.data.response);
-  } catch (error) {
-    errorHandler({
-      status: error.response.status,
-      message: error.response.data.response
-    });
-  }
-})();
+var book = {};
+book[books[0][0]] = books[1][0];
+book[books[0][1]] = books[1][1];
+book[books[0][2]] = books[1][2];
+book[books[0][3]] = books[1][3];
+book[books[0][4]] = books[1][4];
+book[books[0][5]] = books[1][5];
+book[books[0][6]] = books[1][6];
+book[books[0][7]] = books[1][7];
+
+console.log(book);
+
+// for (var i = 1; i < books.length; i++) {
+
+//   axios
+//     .put("http://localhost:3000/api/categories", {
+//       name: books[i].category
+//     })
+//     .then(function(response) {
+//       console.log(response.status);
+//       console.log(response.data);
+//     })
+//     .catch(function(error) {
+//       errorHandler({
+//         status: error.response.status,
+//         message: error.response.data.response
+//       });
+//     });
+// }
