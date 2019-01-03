4c0ce17f9659b5b36c17f2517370356aaa2c5d89
Rana Sobhy <ranasobhy1212@gmail.com>
2019-01-03 22:28:03 +0200

    read from excel file as objects

--- app.js
+++ app.js
@@ -1,22 +1,27 @@
-const xlsx = require("node-xlsx").default;
 const axios = require("axios");
 const errorHandler = require("./Handler/error");
+const excelToJson = require('convert-excel-to-json');
+ 
+const result = excelToJson({
+    sourceFile: 'books.xlsx',
+    columnToKey: {
+        '*': '{{columnHeader}}'
+    }
+});
+const books = result["books"];
+const authors = result["authors"];
 
-const workbook = xlsx.parse(`${__dirname}/books.xlsx`);
-const books = workbook[0].data;
-const authors = workbook[1].data;
+// var book = {};
+// book[books[0][0]] = books[1][0];
+// book[books[0][1]] = books[1][1];
+// book[books[0][2]] = books[1][2];
+// book[books[0][3]] = books[1][3];
+// book[books[0][4]] = books[1][4];
+// book[books[0][5]] = books[1][5];
+// book[books[0][6]] = books[1][6];
+// book[books[0][7]] = books[1][7];
 
-var book = {};
-book[books[0][0]] = books[1][0];
-book[books[0][1]] = books[1][1];
-book[books[0][2]] = books[1][2];
-book[books[0][3]] = books[1][3];
-book[books[0][4]] = books[1][4];
-book[books[0][5]] = books[1][5];
-book[books[0][6]] = books[1][6];
-book[books[0][7]] = books[1][7];
-
-console.log(book);
+// console.log(book);
 
 // for (var i = 1; i < books.length; i++) {
 
