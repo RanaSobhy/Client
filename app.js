ed6d0589f52d137e6b37ad66a820694c4ef52c04
Rana Sobhy <ranasobhy1212@gmail.com>
2019-01-06 12:57:45 +0200

    get author and category id if they already exist

--- app.js
+++ app.js
@@ -13,27 +13,65 @@ const authors = result["authors"];
 
 ( async  () => {
     for (var i = 1; i < books.length; i++) {
-        try{
-            var booksAuthor = authors.find(a=> a.name === books[i].author);
-            var author = await axios
-            .put("http://localhost:3000/api/authors", booksAuthor);
+        var author;
+        var category;
+        var book;
 
-            var category = await axios
-            .put("http://localhost:3000/api/categories", {
-            name: books[i].category
+        var booksAuthor = authors.find(a=> a.name === books[i].author);
+        await axios.put("http://localhost:3000/api/authors", booksAuthor).then(response => {
+            author = response.data.response;
+        },async (error) => {
+            if(error.response.data.response === "Author already exists")
+            {
+                await axios.post("http://localhost:3000/api/authors",{filter: booksAuthor}).then(response =>{
+                    author = response.data.response[0];
+                }, error => {
+                    
+                error.response.data.row = i;
+                errorHandler(error.response.data);
+                });
+            }
+            else{
+                error.response.data.row = i;
+                errorHandler(error.response.data);
+            }
+        });
+
+        if(author)
+        {
+            await axios.put("http://localhost:3000/api/categories", {name: books[i].category}).then(response => {
+                category = response.data.response;
+            },async (error) => {
+                if(error.response.data.response === "Category already exists")
+                {
+                    await axios.post("http://localhost:3000/api/categories",{filter: {name: books[i].category}}).then(response =>{
+                        category = response.data.response[0];
+                    }, error => {
+                        
+                    error.response.data.row = i;
+                    errorHandler(error.response.data);
+                    });
+                }
+                else
+                {
+                    error.response.data.row = i;
+                    errorHandler(error.response.data);
+                }
             });
 
-            books[i].author = author.data.response.id;
-            books[i].category = category.data.response.id;
-
-            var book = await axios
-            .put("http://localhost:3000/api/books", books[i]);
-
-            console.log(book.data);
-        }
-        catch(error){
-            error.response.data.row = i;
-            errorHandler(error.response.data);
+            if(category)
+            {
+                books[i].author = author.id;
+                books[i].category = category.id;
+                await axios.put("http://localhost:3000/api/books", books[i]).then(response => {
+                    book = response.data.response;
+                },error => {
+                    error.response.data.row = i;
+                    errorHandler(error.response.data);
+                });
+                
+                if(book) console.log(book);
+            }   
         }
     }
 })();
